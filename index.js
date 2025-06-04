cd70a858f1795c6c2ab7b84517843c3b133152a0
FeSanz <68288843+FeSanz@users.noreply.github.com>
2025-06-03 18:59:48 -0600

    CORS Render

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
 
-/*app.use(cors({
-    origin: '*',
+app.use(cors({
+    origin: ['http://localhost:8100', 'http://localhost:4200', 'http://localhost:3000'], // Agrega todos tus puertos de desarrollo
     methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
-    allowedHeaders: ['Content-Type']
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors());
-app.use(express.json());*/
-
-app.use(cors());
+app.options('*', cors()); // Para manejar preflight requests
 app.use(express.json());
+
+//app.use(cors());
+//app.use(express.json());
 
 app.use('/api', require('./services/fusion/organizations'));
 
@@ -27,8 +27,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor corriendo en puerto ${port}`);
 });
-
-/*const server = app.listen(port, () => {
-    console.log(`API REST escuchando en http://localhost:${port}`);
-});*/
-
