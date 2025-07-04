c0f1f8f848714aceb24bfd32bf4f8aca03c802e2
BrandoRubio <33293044+BrandoRubio@users.noreply.github.com>
2025-07-04 09:04:19 -0600

    db local

--- database/pool.js
+++ database/pool.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    //ssl: false // 👈 esto evita el error
-    ssl: {
+    ssl: false
+    /*ssl: {
         rejectUnauthorized: false, // Evitar bloqueos en de conexión
-    },
+    },*/
 });
 
 module.exports = pool;
